f3fee88ef99604aa2bc2013ac599cd25f9b687e2
Jon Psaila <jonathan_psaila@outlook.com>
2025-02-16 13:06:05 -0500

    Shell linting

--- system-setup.sh
+++ system-setup.sh
@@ -36,4 +36,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
 cp -f ./config/.zshrc ~/.zshrc
+# shellcheck source=/dev/null
 source "$HOME/.zshrc"
