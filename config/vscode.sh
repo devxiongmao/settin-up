ad6b39240ce767aa5b5b89c76cbd719d1935d52f
Jon Psaila <jonathan_psaila@outlook.com>
2025-02-17 14:19:06 -0500

    Adds ruby extensions for VS Code

--- config/vscode.sh
+++ config/vscode.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+####### Ruby extensions
 code --install-extension castwide.solargraph
+code --install-extension kaiwood.endwise
+code --install-extension bung87.rails
+code --install-extension rebornix.Ruby
+
+####### Javascript extensions
+
+####### Typescript extensions
+
+####### Go extensions
+
+####### Python extensions
