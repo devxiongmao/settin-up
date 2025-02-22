18143b5583cc76066b68f06882c9c33ce45a869d
Jon Psaila <jonathan_psaila@outlook.com>
2025-02-22 00:29:52 -0500

    Adds other extensions to vscode.sh file

--- config/vscode.sh
+++ config/vscode.sh
@@ -7,9 +7,13 @@ code --install-extension bung87.rails
 code --install-extension rebornix.Ruby
 
 ####### Javascript extensions
-
-####### Typescript extensions
-
-####### Go extensions
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
 
 ####### Python extensions
+code --install-extension ms-python.python
+code --install-extension njpwerner.autodocstring
+code --install-extension tamasfe.even-better-toml
+code --install-extension ms-toolsai.jupyter
+code --install-extension KevinRose.vsc-python-indent
+code --install-extension charliermarsh.ruff
